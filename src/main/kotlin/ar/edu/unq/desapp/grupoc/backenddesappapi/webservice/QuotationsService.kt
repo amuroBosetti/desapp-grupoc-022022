08513cdee07d62464b68c84d71ca6718ad536bf6
Mauro Bosetti <mauro.bosetti@10pines.com>
2022-10-05 19:53:40 -0300

    Makes BinanceApiRestClient injectable

--- src/main/kotlin/ar/edu/unq/desapp/grupoc/backenddesappapi/webservice/QuotationsService.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoc/backenddesappapi/webservice/QuotationsService.kt
@@ -1,16 +1,17 @@
 package ar.edu.unq.desapp.grupoc.backenddesappapi.webservice
 
-import com.binance.api.client.BinanceApiClientFactory
 import com.binance.api.client.BinanceApiRestClient
-import com.binance.api.client.domain.DomainType
 import com.binance.api.client.domain.market.TickerPrice
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class QuotationsService {
+
+    @Autowired
+    lateinit var client: BinanceApiRestClient
+
     fun getTokenPrice(ticker: String): TickerPrice {
-        val factory: BinanceApiClientFactory = BinanceApiClientFactory.newInstance(DomainType.Com)
-        val client: BinanceApiRestClient = factory.newRestClient(DomainType.Com)
         return client.getPrice(ticker)
     }
 }
