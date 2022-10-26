f64f4237531972e478984ad0a6dd9270db7eb6ae
Mauro Bosetti <mauro.bosetti@10pines.com>
2022-10-26 09:56:29 -0300

    Fix insert syntax

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,3 @@
-INSERT INTO brokeruser (address, cvu, email, name, password, reputationpoints, surname, walletId)
-VALUES
-('Calle falsa 123, Wilde', '6234964396100474240457', 'mauro.bosetti@gmail.com', 'Mauro', '12345ASDASdsadas', 0.0, 'Bosetti', '43835670'),
-('Calle falsa 123, Wilde', '6234564396100474240457', 'ignacio.robledo@gmail.com', 'Ignacio', '12345ASDASdsadas', 0.0, 'Robledo', '4387670')
+INSERT INTO brokeruser (address, cvu, email, name, password, reputationpoints, surname, walletId) VALUES ('Calle falsa 123, Wilde', '6234964396100474240457', 'mauro.bosetti@gmail.com', 'Mauro', '12345ASDASdsadas', 0, 'Bosetti', '43835670');
+
+INSERT INTO brokeruser (address, cvu, email, name, password, reputationpoints, surname, walletId) VALUES ('Calle falsa 123, Wilde', '6234564396100474240457', 'ignacio.robledo@gmail.com', 'Ignacio', '12345ASDASdsadas', 0, 'Robledo', '4387670');
