9089eeca37a8fe03577f3ef33d408c1f64aa97bb
Mauro Bosetti <mauro.bosetti@10pines.com>
2022-11-23 20:09:50 -0300

    Removes ehcache configuration (in favor of default Spring configuration)

--- src/main/kotlin/ar/edu/unq/desapp/grupoc/backenddesappapi/config/CacheConfig.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoc/backenddesappapi/config/CacheConfig.kt
@@ -1,32 +1,27 @@
 package ar.edu.unq.desapp.grupoc.backenddesappapi.config
 
-import org.ehcache.config.builders.CacheConfigurationBuilder
-import org.ehcache.config.builders.CacheEventListenerConfigurationBuilder
-import org.ehcache.config.builders.CacheManagerBuilder
-import org.ehcache.config.builders.ResourcePoolsBuilder
-import org.ehcache.event.EventType
+import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.concurrent.ConcurrentMapCache
+import org.springframework.cache.support.SimpleCacheManager
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.ResponseEntity
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @Configuration
 @EnableCaching
+@EnableScheduling
 class CacheConfig {
 
-    init {
-        val cacheEventListenerConfiguration = CacheEventListenerConfigurationBuilder
-            .newEventListenerConfiguration(CacheEventLogger(), EventType.CREATED, EventType.UPDATED)
-            .unordered().asynchronous()
-
-        CacheManagerBuilder.newCacheManagerBuilder().withCache(
-            "priceCache",
-            CacheConfigurationBuilder.newCacheConfigurationBuilder(
-                String::class.java,
-                ResponseEntity::class.java,
-                ResourcePoolsBuilder.heap(10)
+    @Bean
+    fun cacheManager(): CacheManager {
+        val cacheManager = SimpleCacheManager()
+        cacheManager.setCaches(
+            listOf(
+                ConcurrentMapCache("priceCache"),
             )
-                .withService(cacheEventListenerConfiguration)
-        ).build(true)
+        )
+        return cacheManager
     }
 
 }
