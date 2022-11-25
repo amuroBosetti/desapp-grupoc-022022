147657b134ccc199b7547d7d74105c6be7483f9b
ignacio-r <ignaciorobledounq@gmail.com>
2022-11-25 17:47:11 -0300

    separate clock config

--- src/main/kotlin/ar/edu/unq/desapp/grupoc/backenddesappapi/BackendDesappApiApplication.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoc/backenddesappapi/BackendDesappApiApplication.kt
@@ -1,9 +1,12 @@
 package ar.edu.unq.desapp.grupoc.backenddesappapi
 
+import ar.edu.unq.desapp.grupoc.backenddesappapi.service.DBInitializerService
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+//@EnableScheduling
 class BackendDesappApiApplication
 
 fun main(args: Array<String>) {
